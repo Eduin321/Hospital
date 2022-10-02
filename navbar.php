1ac27f458d20cd005ece38ff0f1808180ec73bc1
Eduin <eduin_bustamante23191@elpoli.edu.co>
2022-10-02 12:22:27 -0500

    Subiendo archivos

---
+++ navbar.php
@@ -0,0 +1,21 @@
+
+<nav style=" border-radius: 0px; height:20px;background-color:#FFF" class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <p class="navbar-brand">Hola,</font> Has accedido como <?php echo $_SESSION['admty']; ?> <a href="dminlogout.php">cerrar sesión</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+  
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
